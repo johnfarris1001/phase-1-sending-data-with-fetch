34922b58e8301edc6867e9470d8fccb47b457159
John <johnfarris217@gmail.com>
2022-12-29 19:47:39 -0600

    commit tree e189f393518aa4e001324c50a1e2d590f79bd5e1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    const userData = {
+        name: name,
+        email: email,
+    }
+
+    const userObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch("http://localhost:3000/users", userObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            addElement(object.id);
+            addElement(object.name);
+            addElement(object.email);
+        })
+        .catch(function (error) {
+            alert("Error!");
+            addElement(error.message);
+        });
+}
+
+function addElement(element) {
+    const p = document.createElement('p');
+    p.innerHTML = element;
+    document.body.appendChild(p);
+}
